b7d2170712d6d6a194ec3f13cdd2c1155d34a422
Mohamed.Irfan.git <irfanmt29@gmail.com>
2025-02-28 14:33:05 +0530

    commit

--- src/Suggestios.jsx
+++ src/Suggestios.jsx
@@ -1,10 +1,74 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 function Suggestios() {
+  const [profile, setProfile] = useState(null)
+  const [suggestions, setSuggestions] = useState([])
+
+
+  useEffect(() => {
+    fetch('http://localhost:3000/profile')
+      .then((data) => data.json())
+      .then(data => setProfile(data)).
+      catch(err => console.log(err))
+
+
+    fetch('http://localhost:3000/suggestions')
+      .then((data) => data.json())
+      .then(data => setSuggestions(data)).
+      catch(err => console.log(err))
+  }, [])
+  console.log(suggestions)
+
+
+
   return (
     <div>
-      suggestions
+      <div className='suggestion m-4'>
+
+        {profile ? (
+          <div className='d-flex'>
+            <img className="dp rounded-circle" src={profile.profileImage} alt="No profile" />
+            <h5>{profile.username}</h5>
+            <small className='ms-auto text-primary'>Switch</small>
+          </div>)
+          : <p>Loading....</p>}
+        <div className='d-flex'>
+          <p>Suggest for you</p>
+
+
+          <p className='ms-auto'><b>See all</b></p>
+
+        </div>
+
+
+        {suggestions.length > 0 ? (
+          <div >
+            {suggestions.map((suggestion) => (
+              <div key={suggestion.userId}>
+                <div className='d-flex gap-3'>
+                  <img className="dp rounded-circle" src={suggestion.profileImage} alt="No profile" />
+                  <h5>{suggestion.username}</h5>
+                  <p className='btn btn-primary ms-auto'>Follow</p>
+                </div>
+
+              </div>
+
+            ))}
+
+
+          </div>
+        ) : (
+          <div>
+            Loading....
+          </div>
+        )
+        }
+
+      </div>
+
     </div>
+
+
   )
 }
 
